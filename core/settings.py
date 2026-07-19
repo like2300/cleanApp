969186713160ee3aa9f9659bef814a94dc604762
like2300 <elengaomerfils@gmail.com>
2026-07-19 23:51:03 +0100

    app execut 2

--- core/settings.py
+++ core/settings.py
@@ -172,24 +172,43 @@ LIVE_MODE = _env("LIVE_MODE", "AUTO")
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": RUNTIME_DIR / "db.sqlite3",
-    },
-    "cloud": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": _env("CLOUD_DB_NAME", "dhavant_db"),
-        "USER": _env("CLOUD_DB_USER", "dhavant"),
-        "PASSWORD": _env("CLOUD_DB_PASSWORD", "Brazzaville0!"),
-        "HOST": _env("CLOUD_DB_HOST", "mysql-dhavant.alwaysdata.net"),
-        "PORT": _env("CLOUD_DB_PORT", "3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-            "charset": "utf8mb4",
-        },
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+# 1. Définition des profils de bases de données disponibles
+SQLITE_DB = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": RUNTIME_DIR / "db.sqlite3",
+}
+
+MYSQL_DB = {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": _env("CLOUD_DB_NAME", "dhavant_db"),
+    "USER": _env("CLOUD_DB_USER", "dhavant"),
+    "PASSWORD": _env("CLOUD_DB_PASSWORD", "Brazzaville0!"),
+    "HOST": _env("CLOUD_DB_HOST", "mysql-dhavant.alwaysdata.net"),
+    "PORT": _env("CLOUD_DB_PORT", "3306"),
+    "OPTIONS": {
+        "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        "charset": "utf8mb4",
     },
 }
+
+# 2. Configuration dynamique selon l'environnement (Production vs Local)
+# Si DEBUG est False (ou si un flag spécifique en .env l'impose), on force MySQL par défaut.
+IS_PRODUCTION = not DEBUG
+
+if IS_PRODUCTION:
+    DATABASES = {
+        "default": MYSQL_DB,
+        "cloud": MYSQL_DB,  # Évite les erreurs si le SyncRouter cible explicitement "cloud"
+    }
+else:
+    DATABASES = {
+        "default": SQLITE_DB,
+        "cloud": MYSQL_DB,
+    }
 
 
 # Password validation
